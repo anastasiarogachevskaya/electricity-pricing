3b888dc469a7b0d2559da8b260d7ca39832b8d05
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-05-04 16:53:49 +0300

    Save state

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { getElectricityPrice } from './api';
+import Wrapper from './components/Wrapper';
+import PriceText from './components/PriceText';
 
-export default function App() {
+function App() {
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {
+    const fetchPrice = async () => {
+      const currentDate = new Date();
+      const date = currentDate.toISOString().split('T')[0];
+      const hour = currentDate.getHours();
+    
+      const data = await getElectricityPrice(date, hour);
+      setPrice(data?.price);
+    };
+    fetchPrice();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Wrapper price={price}>
+      <PriceText>{price}</PriceText>
+    </Wrapper>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
