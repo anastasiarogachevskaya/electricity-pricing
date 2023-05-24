90b463f2d179ce02d425de709858679e0984bb5f
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-05-24 23:35:47 +0300

    Added google fonts, split screens

--- components/StyledImage.tsx
+++ components/StyledImage.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components/native';
 
 const StyledImage = styled.Image`
   max-width: 80px;
-  max-height: 150px;
+  max-height: 155px;
   transform: rotate(-90deg);
+  flex-grow: 5;
 `;
 
 export default StyledImage;
